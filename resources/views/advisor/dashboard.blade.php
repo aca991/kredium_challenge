8bdb808a575f118722794954a3fa4b2ef51e4fd3
adjordjevic <adjordjevic991@gmail.com>
2024-11-17 22:31:18 +0100

    Typo

--- resources/views/advisor/dashboard.blade.php
+++ resources/views/advisor/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
     <div class="content-container dashboard-container">
-        <a class="dashboard-link" href="{{ $viewClientsRoute }}">View Clients</a>
+        <a class="dashboard-link" href="{{ $viewClientsRoute }}">View All Clients</a>
         <a class="dashboard-link" href="{{ $viewReportRoute }}">View Report</a>
         @include('header.logout-form')
     </div>
